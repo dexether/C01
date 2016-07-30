64aaac5c97fd7f7bcc2834b863fe9459b6dfcd9c
Tarikh D. Agustia <tarikh@si.co.id>
2016-07-30 23:01:33 +0700

    tambahan texts

--- application/language/indonesia/message_lang.php
+++ application/language/indonesia/message_lang.php
@@ -10,8 +10,40 @@ $lang["header_menu_contact"] = "Hubungi Kami";
 $lang["header_menu_faq"] = "Tanya Jawab";
 $lang["header_menu_help"] = "Bantuan";
 $lang["header_menu_sign"] = "Masuk";
+$lang["header_menu_myagenda"] = "AgendaKu";
+$lang["header_menu_logout"] = "Keluar";
 $lang["header_menu_register"] = "Buat Akun";
 $lang["header_field_seacrh"] = "Cari yang anda mau disini ...";
 
 $lang["page_404_title"] = "Maaf, halaman yang anda cari tidak ditemukan.";
 $lang["page_404_button_title"] = "Kembali keberanda.";
+
+/* Main Body */
+
+$lang["main_daily_promo_title"] = "Promo Hari ini";
+$lang["main_best_ea"] = "Terbaik dari EA";
+$lang["main_best_signal"] = "Terbaik dari Signal";
+$lang["main_best_book"] = "Terbaik dari Buku";
+$lang["main_best_seminar"] = "Terbaik dari Seminar";
+$lang["main_best_copytrade"] = "Terbaik dari Copytrade";
+$lang["main_best_education"] = "Terbaik dari Edukasi";
+$lang["main_best_merch"] = "Terbaik dari Merchandise";
+
+/* End Of Main Body */
+
+/* Start Of Cart */
+$lang["cart_title"] = "Keranjang Belanja Saya";
+$lang["cart_qty"] = "Qty barang";
+$lang["cart_total"] = "Total";
+$lang["cart_checkout"] = "Bayar";
+$lang["cart_continue"] = "Lanjutkan Berbelanja";
+$lang["cart_empty"] = "Anda belum memiliki item apapun";
+$lang["cart_empty_button"] = "Mulai Berbelanja";
+/* End Of Cart */
+
+/* Start of Checkout */
+$lang["checkout_title"] = "Checkout Pesanan";
+$lang["checkout_order_info"] = "Daftar Pesanan";
+$lang["checkout_alamat_info"] = "Detail penagihan";
+$lang["checkout_payment_info"] = "Pembayaran";
+/* End of Checkout */
