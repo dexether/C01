6d458f72bd6b1485332ce6205eb111347b73b59d
Tarikh D. Agustia <tarikh@si.co.id>
2016-08-15 15:47:58 +0700

    Two Forex Agent registration

--- web2/custom/js/ar_account_mm.js
+++ web2/custom/js/ar_account_mm.js
@@ -14,13 +14,12 @@ var ar_account_mm_JS = function() {
                 type: 'POST',
                 dataType: 'json',
                 success: function(response) {
-                   // console.log(response)
-                   var res = response;
-                   swal(res.subject, res.msg, res.status);
-                   // console.log(res);
-                   
+                    // console.log(response)
+                    var res = response;
+                    swal(res.subject, res.msg, res.status);
+                    // console.log(res);
                 },
-                error: function(response){
+                error: function(response) {
                     swal('Oops, something was happend', 'Contact Administrator', 'error');
                 }
             });
@@ -241,7 +240,39 @@ var ar_account_mm_JS = function() {
             }, function() {
                 setTimeout(function() {
                     $.ajax({
-                        url: 'imp_agent_do.php',
+                        url: 'imp_agent_do.php?type=askap',
+                        type: 'POST',
+                        data: data,
+                        dataType: 'JSON',
+                    }).done(function(res) {
+                        // console.log(res)
+                        swal(res.subject, res.msg, res.status);
+                        // setTimeout('history.go(0)', 4000)
+                    }).fail(function() {
+                        swal('We found an error', 'Check your details', 'error');
+                    }).always(function() {});
+                    // swal("Success!", "The job have running well.", "success");
+                    // Ajax Start
+                }, 1000);
+            });
+        },
+        create_agent_two: function(a) {
+            var data = a.serializeArray();
+            // console.log(data);
+            swal({
+                title: "Are you sure?",
+                text: "Do you want to process ?",
+                type: "info",
+                confirmButtonText: "Yes, process it!",
+                cancelButtonText: "No, cancel!",
+                showCancelButton: true,
+                closeOnConfirm: false,
+                html: true,
+                showLoaderOnConfirm: true,
+            }, function() {
+                setTimeout(function() {
+                    $.ajax({
+                        url: 'imp_agent_do.php?type=twofrx',
                         type: 'POST',
                         data: data,
                         dataType: 'JSON',
