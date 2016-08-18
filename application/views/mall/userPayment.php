61b84202e0463fa5090b44d9289b4808a348749c
Tarikh D. Agustia <tarikh@si.co.id>
2016-08-18 08:57:07 +0700

    transactional

---
+++ application/views/mall/userPayment.php
@@ -0,0 +1,50 @@
+<div class="container">
+    <header class="page-header">
+        <h1 class="page-title">Transaksi Anda</h1>
+    </header>
+    <div class="gap gap-small"></div>
+    <div class="row" data-gutter="60">
+        <div class="col-md-12">
+            <!-- <form class="form-inline" role="form">
+              <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd">
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form> -->
+        <div class="gap-small"></div>
+        <hr/>
+        <br/>
+        <table  cellspacing="" cellpadding="1" class="table table-responsive">
+            <thead>
+                <tr>
+                    <th>Invoice</th>
+                    <th>Total</th>
+                    <th>Time</th>
+                    <th>Status</th>
+                    <th>Tindakan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Invoice</td>
+                    <td>Total</td>
+                    <td>Time Update</td>
+                    <td>Status</td>
+                    <td>Tindakan</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+</div>
+
+<div class="gap"></div>
