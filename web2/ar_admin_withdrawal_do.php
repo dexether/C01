d74d6682152e706f0490576928d570d800366516
Fadli Yunus <fyunus70@gmail.com>
2017-01-27 14:25:48 +0700

    bonus withdrawal 3% rubah data di ar_admin_withdrawal_do

--- web2/ar_admin_withdrawal_do.php
+++ web2/ar_admin_withdrawal_do.php
@@ -45,6 +45,54 @@ foreach ($result as $rows) {
    $type     = $rows['type_transaction'];
 }
 
+/*  Cari Upline */
+
+    $condiional_header = "";
+    $condiional_footer = "";
+
+
+$query = "SELECT
+		  client_aecode.name,
+		  client_aecode.email,
+		  client_accounts.`accountname`,
+		  mlm.`ACCNO`
+		FROM
+		  client_aecode,
+		  client_accounts,
+		  mlm
+		WHERE client_aecode.`aecodeid` = client_accounts.`aecodeid`
+		  AND client_accounts.`accountname` = mlm.`ACCNO`
+		  AND mlm.`ACCNO` = '$acc_from' ";
+$datatress = array();
+$rows = $DB->execresultset($query);
+foreach ($rows as $row) {
+    $datatress[$row['ACCNO']] = $row;
+}
+// var_dump($query);
+$longtree = $condiional_header;
+if (count($datatress) > 0) {
+    foreach ($datatress AS $Upline => $datatres) {
+		$key = 0;
+        $longtree = $longtree .$Upline;
+        $longtree = updatechild($longtree, $Upline);		
+        $longtree = $longtree ;
+		$key = $key + 1;
+    }
+}
+$longtree = $longtree . $condiional_footer;
+$template->assign("longtree", $longtree);
+// var_dump($longtree);
+$data = (explode(",",$longtree));
+$hitung=count($data);
+if ($hitung <= 7 ){
+	$i = $hitung - 1;
+}else{
+	$i = 6;
+}
+// var_dump($i);
+// var_dump($data);
+/* End Cari Upline */
+
 $query = "SELECT value FROM app_config WHERE `key` = 'AR_WITHDRAWAL_TAX'";
 			$result = $DB->execresultset($query);
 			$tax = '';
@@ -118,6 +166,97 @@ if ($errno == 0) {
             $error   = "success";
             $subject = "Succes";
             $msg     = "The request has been approved";
+			
+			For($a=1;$a<=$i;$a++){
+			  $query = "SELECT 
+						  mlm.*,
+						  mlm_bonus_settings.`lv`,
+						  client_accounts.`suspend`
+						FROM
+						  mlm,
+						  mlm_bonus_settings,
+						  client_accounts
+						WHERE mlm.`group_play`=mlm_bonus_settings.`group_play`
+						AND mlm.`ACCNO`=client_accounts.`accountname`
+						AND mlm.`ACCNO`='$data[$a]'";
+						$uplinenya = array();
+						$result = $DB->execresultset($query);
+						foreach ($result as $row) {
+							$uplinenya = $row;
+							$uplinenya['bw'] = $amount*3/100 ;
+						}
+						// var_dump($query);
+						// var_dump($uplinenya['bw']);
+				if ($uplinenya['suspend']=='0'){
+					if ($uplinenya['lv'] >= $a ){
+						$query = "SELECT * FROM mlm_ewallet WHERE mlm_ewallet.`account`='$data[$a]'";
+						$dataewall = array();
+						$result = $DB->execresultset($query);
+						foreach ($result as $row) {
+							$dataewall = $row;
+							}
+						// var_dump($dataewall);
+						// var_dump($dataewall['balance']);
+
+						$newbalace =$dataewall['balance']+$uplinenya['bw'];
+						// var_dump($newbalace);
+						
+						$timeupdate = date('Y-m-d H:i:s', strtotime('-1 hour'));
+						$query   = "UPDATE 
+									  mlm_ewallet 
+									SET
+									  balance = '$newbalace', 
+									  balance_prev = '$dataewall[balance]',
+									  lastupdate = '$timeupdate',
+									  lastupdate_prev = '$dataewall[lastupdate]'
+									WHERE mlm_ewallet.`account`='$data[$a]'";
+						$DB->execonly($query);
+						// var_dump($query);
+
+						$query = "SELECT 
+						  client_aecode.`aecode`,
+						  client_aecode.`name` 
+						FROM
+						  client_aecode,
+						  client_accounts 
+						WHERE client_aecode.`aecodeid` = client_accounts.`aecodeid`
+						  AND client_accounts.`accountname`='$data[$a]'";
+						$name = '';
+						$aecode = '';
+						$result = $DB->execresultset($query);
+						foreach ($result as $row) {
+							$aecode = $row['aecode'];
+							$name = $row['name'];
+						}
+						// var_dump($query);
+
+						// Send Email
+						$subject = "Congratulations, Your bonus increases ";
+						$to = $aecode;
+						$body = "Time: " . date('Y-m-d H:i:s', strtotime('-1 hour')) . "<br> <br>";
+						$body = $body . "Dear ".$name.",<br>";
+						$body = $body . " <br>";
+						$body = $body . "Congratulations, Your bonus increases<br>";
+						$body = $body . "Because your downline make withdrawals from their accounts<br>";
+						// $body = $body . " <br>";
+						// $body = $body . "Congratulations, Your bonus increases of bonus withdrawal<br>";
+						$body = $body . " <br>";
+						$body = $body . " <br>";
+						$body = $body . "Thank you," . "<br>";
+						$body = $body . "<br><strong>".$companys['companyname']."</strong>" . "<br>";
+						$body = $body . $companys['long_address'];
+						$body = $body . " Email : ".$companys['email']." <br>";
+						$body = $body . " ".$companys['companyurl']." <br>";
+
+						sendEmail($to, $subject, $body , 'ar_subadmin_link');
+						bonusLogs($data[$a], 'wdd', $uplinenya['bw'], 'you got Withdrawal Commision bonus From '.$acc_from.' amounted '.$uplinenya['bw'].'');
+					}
+					
+				}
+						
+				
+			}
+			
          } elseif ($postmode == 'pending') {
             $query = "UPDATE mlm_transaction SET status = '1' WHERE id = '$id'";
             $DB->execonly($query);
@@ -185,6 +324,72 @@ $response = array('status' => $error, 'subject' => $subject, 'msg' => $msg);
 echo json_encode($response);
 
 /*=====  End of coding  ======*/
+function updatechild($longtree, $Upline2) {
+
+  $longtree = $longtree . "";
+	global $user;
+    global $DB;
+    $datatress = array();
+    $query = "SELECT
+		  client_aecode.name,
+		  client_aecode.email,
+		  client_accounts.`accountname`,
+		  mlm.`Upline`
+		FROM
+		  client_aecode,
+		  client_accounts,
+		  mlm
+		WHERE client_aecode.`aecodeid` = client_accounts.`aecodeid`
+		  AND client_accounts.`accountname` = mlm.`ACCNO`
+		  AND mlm.`Upline` <> 'COMPANY'
+		  AND mlm.`ACCNO` = '$Upline2' ";
+
+    $rows = $DB->execresultset($query);
+    foreach ($rows as $row) {
+        $datatress[$row['Upline']] = $row;
+    }
+	// var_dump($datatress);
+	// $hitung=count($datatress);
+	$query = "SELECT
+	mlm.`ACCNO`,
+	mlm.`Upline`
+	FROM
+	mlm,
+	client_accounts
+	WHERE mlm.`ACCNO`=client_accounts.`accountname`
+	AND mlm.`group_play`='Car'
+	AND client_accounts.`email` ='$user->username' ";
+
+	$rows = $DB->execresultset($query);
+	$bm="";
+	foreach ($rows as $row) {
+		$bm = $row['Upline'];
+	}
+		// var_dump($bm);
+
+    if (count($datatress) > 0) {
+		
+        foreach ($datatress AS $Upline1 => $datatres) {
+			if ($bm != $Upline1){
+				$longtree = $longtree . "," . $Upline1;
+				$longtree = updatechild($longtree, $Upline1);
+				$longtree = $longtree;
+				// $hitung=count($longtree);
+				// var_dump($hitung);
+			}
+
+        }
+    }
+    $longtree = $longtree . "";
+	 // var_dump($longtree);
+    return $longtree;
+}
+
+function bonusLogs($account, $type, $amount, $comment) {
+	global $DB;
+	$query = "INSERT INTO mlm_bonus_logs SET account = '$account', bonus_type = '$type', amount = '$amount', comment = '$comment', date_receipt = NOW()";
+	$DB->execonly($query);
+}
 
 function getIdentitas($account)
 {
